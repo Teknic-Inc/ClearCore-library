8e0b4dfc13b606a22842fbe5574a5dbd7bc225d9
TEKNIC\allen_wells <allen_wells@teknic.com>
2020-10-30 13:58:17 -0400

    Reverted changes to ReadDigitalInput example

--- Atmel_Examples/DigitalIOExamples/ReadDigitalInput/ReadDigitalInput.cpp
+++ Atmel_Examples/DigitalIOExamples/ReadDigitalInput/ReadDigitalInput.cpp
@@ -1,15 +1,16 @@
 /*
- * Title: SDCardReadWriteTest
+ * Title: ReadDigitalInput
  *
  * Objective:
- *    This example demonstrates how to use the reading and writing functionality
- *    of the ClearCore SD card reader.
+ *    This example demonstrates how to read the state of a ClearCore digital
+ *    input.
  *
  * Description:
- *    This example reads from and writes to a .txt file
+ *    This example repeatedly reads the state of a defined digital input. During
+ *    operation, the state of the input is printed to the USB serial port.
  *
  * Requirements:
- * ** A USB serial connection to the a ClearCore, An SD card inserted into the ClearCore's SD card reader
+ * ** A digital input device, such as a switch or sensor, connected to DI-6
  *
  * Links:
  * ** ClearCore Documentation: https://teknic-inc.github.io/ClearCore-library/
@@ -21,74 +22,48 @@
  */
 
 #include "ClearCore.h"
-#include "SdFat.h"
-#include "FatFile.h"
 
+// Specify which input pin to read from.
+// IO-0 through A-12 are all available as digital inputs.
+#define inputPin ConnectorDI6
+
+// The current state of the input pin
+int16_t state;
+
+// Select the baud rate to match the target serial device.
+#define baudRate 9600
+
+// Specify which serial to use: ConnectorUsb, ConnectorCOM0, or ConnectorCOM1.
 #define SerialPort ConnectorUsb
 
-// SD chip select pin
-const uint8_t chipSelect = CLEARCORE_PIN_INVALID;
-
-// File size in MB where MB = 1,000,000 bytes.
-const uint32_t FILE_SIZE_MB = 5;
-
-//==============================================================================
-// End of configuration constants.
-//------------------------------------------------------------------------------
-
-// test file
-FatFile file;
-
 int main() {
-	//Set up serial communication at a baud rate of 9600 bps then wait up to
-	//5 seconds for a port to open.
-	//ConnectorUsb communication is not required for this example to run, however the
-	//example will appear to do nothing without serial output.
-	ConnectorUsb.Mode(Connector::USB_CDC);
-	ConnectorUsb.Speed(9600);
-	uint32_t timeout = 5000;
-	uint32_t startTime = Milliseconds();
-	ConnectorUsb.PortOpen();
-	while (!ConnectorUsb && Milliseconds() - startTime < timeout) {
-		continue;
-	}
-	Delay_ms(1000);
-	SerialPort.SendLine("Initializing SD card...");
-
-	//Initialize SD variables:
-	SdFat SD;
-	FatFile myFile;
-    uint8_t buf[1024];
-    for(int i = 0; i<sizeof(buf);i++){
-        buf[i] = 0;
+    // Set up serial communication at a baud rate of 9600 bps then wait up to
+    // 5 seconds for a port to open.
+    // Serial communication is not required for this example to run, however the
+    // example will appear to do nothing without serial output.
+    SerialPort.Mode(Connector::USB_CDC);
+    SerialPort.Speed(baudRate);
+    uint32_t timeout = 5000;
+    uint32_t startTime = Milliseconds();
+    SerialPort.PortOpen();
+    while (!SerialPort && Milliseconds() - startTime < timeout) {
+        continue;
     }
-	
-	if (!SD.begin()) {
-		SerialPort.SendLine("initialization failed!");
-		return 0;
-	}
-	SerialPort.SendLine("initialization done.");
 
-	// open the file. note that only one file can be open at a time,
-	// so you have to close this one before opening another.
+    while (true) {
+        // Read the state of the input connector.
+        state = inputPin.State();
 
-	// re-open the file for reading:
-	myFile.open("TEST.txt");
-	if (myFile.isOpen()) {
-		SerialPort.SendLine("TEST.txt:");
+        // Display the state of the input connector.
+        SerialPort.Send("DI-6 Input state: ");
+        if (state) {
+            SerialPort.SendLine("ON");
+        }
+        else {
+            SerialPort.SendLine("OFF");
+        }
 
-		// read from the file until there's nothing else in it:
-            myFile.readASync(buf,sizeof(buf));
-            Delay_ms(1000);
-            for(int i = 0; i<sizeof(buf);i++){
-                SerialPort.Send((char)buf[i]);
-            }
-		// close the file:
-		myFile.close();
-		} else {
-		// if the file didn't open, print an error:
-		SerialPort.SendLine("error opening test.txt");
-	}
-	return 0;
-
+        // Wait a second then repeat...
+        Delay_ms(1000);
+    }
 }
