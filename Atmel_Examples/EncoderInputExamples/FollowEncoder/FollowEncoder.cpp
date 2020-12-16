3afa15bcc90655bd0902ed2d9cc025f712f63511
Zachary Lebold <zach_lebold@teknic.com>
2020-12-16 14:00:27 -0500

    Added FollowEncoder example to highlight EncoderInput features.

---
+++ Atmel_Examples/EncoderInputExamples/FollowEncoder/FollowEncoder.cpp
@@ -0,0 +1,149 @@
+/*
+ * Title: FollowEncoder
+ *
+ * Objective:
+ *    This example demonstrates the ClearCore's Encoder Input module
+ *    functionality.
+ *
+ * Description:
+ *    This example takes input signals from an external encoder and uses the
+ *    captured position/velocity to control a ClearPath motor.
+ *
+ * Requirements:
+ * 1. A ClearPath motor must be connected to Connector M-0.
+ * 2. The connected ClearPath motor must be configured through the MSP software
+ *    for Step and Direction mode (In MSP select Mode>>Step and Direction).
+ * 3. The ClearPath motor must be set to use the HLFB mode "ASG-Position"
+ *    through the MSP software (select Advanced>>High Level Feedback [Mode]...
+ *    then choose "All Systems Go (ASG) - Position" from the dropdown and hit
+ *    the OK button).
+ * 4. Set the Input Format in MSP for "Step + Direction".
+ * 5. The A and B signals from an external encoder source must be connected to
+ *    inputs DI-6 and DI-7. (The exact ordering doesn't matter.)
+ * 6. The index signal from an external encoder source must be connected to
+ *    input DI-8.
+ *
+ * ** Note: Homing is optional, and not required in this operational mode or in
+ *    this example. This example makes its first move in the positive direction,
+ *    assuming any homing move occurs in the negative direction.
+ *
+ * ** Note: Set the Input Resolution in MSP the same as your motor's Positioning
+ *    Resolution spec if you'd like the pulses sent by ClearCore to command a
+ *    move of the same number of Encoder Counts, a 1:1 ratio.
+ *
+ * Links:
+ * ** ClearCore Documentation: https://teknic-inc.github.io/ClearCore-library/
+ * ** ClearCore Manual: https://www.teknic.com/files/downloads/clearcore_user_manual.pdf
+ * ** ClearPath Manual (DC Power): https://www.teknic.com/files/downloads/clearpath_user_manual.pdf
+ * ** ClearPath Manual (AC Power): https://www.teknic.com/files/downloads/ac_clearpath-mc-sd_manual.pdf
+ *
+ * 
+ * Copyright (c) 2020 Teknic Inc. This work is free to use, copy and distribute under the terms of
+ * the standard MIT permissive software license which can be found at https://opensource.org/licenses/MIT
+ */
+
+#include "ClearCore.h"
+
+
+// Set the operational mode of this example:
+// If true, the motor will faithfully follow the encoder's position.
+// If false, the motor will faithfully follow the encoder's velocity instead.
+bool followPosition = false;
+
+// Define the velocity and acceleration limits to be used for positional moves
+int32_t velocityLimit = 100000; // pulses per sec
+int32_t accelerationLimit = 1000000; // pulses per sec^2
+
+// Set to true if the sense of encoder direction should be inverted.
+bool swapDirection = false;
+// Set to true if index detection should occur on the falling edge,
+// rather than the rising edge.
+bool indexInverted = false;
+
+int main(void) {
+    // Set up serial communication and wait up to 5 seconds for a port to open
+    uint32_t timeout = 5000;
+    uint32_t startTime = Milliseconds();
+    ConnectorUsb.PortOpen();
+    while (!ConnectorUsb && Milliseconds() - startTime < timeout) {
+        continue;
+    }
+
+    // Enable the encoder input feature
+    EncoderIn.Enable(true);
+    // Zero the position to start
+    EncoderIn.Position(0);
+    // Set the encoder direction
+    EncoderIn.SwapDirection(swapDirection);
+    // Set the sense of index detection (true = rising edge, false = falling edge)
+    EncoderIn.IndexInverted(indexInverted);
+
+    // Motor setup:
+    MotorMgr.MotorModeSet(MotorManager::MOTOR_ALL, Connector::CPM_MODE_STEP_AND_DIR);
+    ConnectorM0.VelMax(velocityLimit);
+    ConnectorM0.AccelMax(accelerationLimit);
+
+    // Enables the motor; homing will begin automatically if enabled
+    ConnectorM0.EnableRequest(true);
+    ConnectorUsb.SendLine("Motor Enabled");
+
+    // Waits for HLFB to assert (waits for homing to complete if applicable)
+    ConnectorUsb.SendLine("Waiting for HLFB...");
+    while (ConnectorM0.HlfbState() != MotorDriver::HLFB_ASSERTED) {
+        continue;
+    }
+    ConnectorUsb.SendLine("Motor Ready");
+
+    // Variables to store encoder state
+    int32_t position = 0;
+    int32_t velocity = 0;
+    int32_t indexPosition = 0;
+    int32_t lastIndexPosition = 0;
+    bool quadratureError = false;
+
+    // Use a timeout to print out encoder information every 500 ms.
+    char info[100];
+    timeout = 500;
+    startTime = Milliseconds();
+
+    while (!quadratureError) {
+        position = EncoderIn.Position();
+        velocity = EncoderIn.Velocity();
+        indexPosition = EncoderIn.IndexPosition();
+        quadratureError = EncoderIn.QuadratureError();
+
+        // Print out encoder info at a fixed timeout rate
+        if (Milliseconds() - startTime >= timeout) {
+            if (followPosition) {
+                snprintf(info, 100, "Encoder position: %ld counts", position);
+            }
+            else {
+                snprintf(info, 100, "Encoder velocity: %ld counts/sec", velocity);
+            }
+            ConnectorUsb.SendLine(info);
+            startTime = Milliseconds();
+        }
+
+        if (indexPosition != lastIndexPosition) {
+            snprintf(info, 100, "Detected index at position: %ld",
+            EncoderIn.IndexPosition());
+            ConnectorUsb.SendLine(info);
+        }
+
+        lastIndexPosition = indexPosition;
+
+        if (followPosition) {
+            // Move the motor to the current position read by the encoder
+            ConnectorM0.Move(position, MotorDriver::MOVE_TARGET_ABSOLUTE);
+        }
+        else {
+            // Command the motor to follow the encoder's velocity
+            ConnectorM0.MoveVelocity(velocity);
+        }
+    }
+
+    // We detected a quadrature error!
+    ConnectorM0.MoveVelocity(0);
+    ConnectorM0.EnableRequest(false);
+    ConnectorUsb.SendLine("Quadrature error detected. Stopping motion...");
+}
