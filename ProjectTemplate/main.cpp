f44214ac6afcaf79a1a95c815eff650147a56733
Nick Dawson <nick_dawson@teknic.com>
2024-08-13 10:52:18 -0400

    link to docs in project template

--- ProjectTemplate/main.cpp
+++ ProjectTemplate/main.cpp
@@ -1,6 +1,11 @@
 
 #include "ClearCore.h"
 
+/**
+ * Reference https://teknic-inc.github.io/ClearCore-library/_microchip_install.html
+ * for more information about how this ProjectTemplate should be used.
+ */
+
 int main(void) {
     /* Replace with your application code below */
     bool ledState = true;
